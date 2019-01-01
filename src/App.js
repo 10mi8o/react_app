f7eeec6fab003e05c7d0c9c7fd53b67bf0d7a4d1
10mi8o <totoma855@gmail.com>
2019-01-01 12:00:18 +0900

    syntax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
+
 class App extends Component {
-  render() {
-    return <div><h1>Hello World!!</h1></div>;
+  render(){
+    return(
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input id="bar" type="text" onChange={() => console.log('hi')} />
+    </React.Fragment>
+    )
   }
 }
+
 
 export default App;
